71b4b9c74b964a61d7829bfad3ae21dbb9c7275f
Bara Yaish <baraihab49@gmail.com>
2022-11-14 21:50:39 +0800

    Imported Regex module to read from file and output

---
+++ index.py
@@ -0,0 +1,21 @@
+import re
+
+# Read the contents of index.txt file
+file = open("index.txt", "r")
+
+# Loop through each line of the index.txt file
+# Store the strings after each space (\s) in each line
+requirements = []
+for line in file:
+    requirements.append(re.split("\s", line))
+
+# Checking the education level by specifying the index
+if (requirements[3][0] == "Education:"):
+    if(requirements[3][1] == "Diploma"):
+        print("Diploma Found.")
+    else:
+        print("Diploma Not Found.")
+else:
+    print("Education was not found.")
+
+print(requirements)
